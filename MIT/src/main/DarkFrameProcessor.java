477f84e7fb12fd7bdba46397fd874395cf940e6b
tgthormodsgaard <tgthormodsgaard@gmail.com>
2019-05-14 22:04:15 -0500

    Small refactor and add comments and a new test.

--- MIT/src/main/DarkFrameProcessor.java
+++ MIT/src/main/DarkFrameProcessor.java
@@ -8,6 +8,7 @@ public class DarkFrameProcessor {
 
     /**
      * Get the average pixel value of an image
+     *
      * @param image double array of the image
      * @return double value of thee average
      */
@@ -15,35 +16,63 @@ public class DarkFrameProcessor {
         return Arrays.stream(image).flatMapToDouble(Arrays::stream).average().getAsDouble();
     }
 
+    /**
+     * Get the max value of a pixel of an image
+     *
+     * @param image double array of the image
+     * @return double value of the max pixel
+     */
     public double getMaxPixelValue(double[][] image) {
         return Arrays.stream(image).flatMapToDouble(Arrays::stream).max().getAsDouble();
     }
 
+    /**
+     * Get the min value of a pixel of an image
+     *
+     * @param image double array of the image
+     * @return double value of the min pixel
+     */
     public double getMinPixelValue(double[][] image) {
         return Arrays.stream(image).flatMapToDouble(Arrays::stream).min().getAsDouble();
     }
 
+    /**
+     * Take an array list of images and process them to create a master dark frame
+     *
+     * @param images a list of images
+     * @return double array of the master dark frame
+     */
     public double[][] createDarkFrame(List<double[][]> images) {
         double[][] darkFrame = new double[images.get(0).length][images.get(0)[0].length];
-        for (double[][] image : images) {
-            for(int i=0; i < image.length; i++) {
-                for(int j=0; j < image[i].length; j++) {
+        images.forEach(image -> {
+            for (int i = 0; i < image.length; i++) {
+                for (int j = 0; j < image[i].length; j++) {
                     darkFrame[i][j] += image[i][j];
                 }
             }
-        }
-        for (int x=0; x < darkFrame.length; x++) {
-            for(int y=0; y < darkFrame[x].length; y++) {
+        });
+
+        for (int x = 0; x < darkFrame.length; x++) {
+            for (int y = 0; y < darkFrame[x].length; y++) {
                 darkFrame[x][y] = darkFrame[x][y] / images.size();
             }
         }
         return darkFrame;
     }
 
+    /**
+     * Take an image and the master dark frame and apply the master dark frame to the provided image
+     *
+     * @param image the image that is to be processed
+     * @param darkFrame the master dark frame
+     * @return double array of the processed image
+     */
     public double[][] applyDarkFrame(double[][] image, double[][] darkFrame) {
         double[][] results = new double[image.length][image[0].length];
-        for(int i=0; i < image.length; i++) {
-            for(int j=0; j <image[i].length; j++) {
+        for (int i = 0; i < image.length; i++) {
+            for (int j = 0; j < image[i].length; j++) {
+                //With this subtraction, you can recieve negatives, but that should be ok as we assume the original
+                // image could have negative values also.  We are just removing the dark frame from the image.
                 results[i][j] = image[i][j] - darkFrame[i][j];
             }
         }
--- MIT/src/test/DarkFrameProcessorTest.java
+++ MIT/src/test/DarkFrameProcessorTest.java
@@ -167,4 +167,74 @@ public class DarkFrameProcessorTest {
 
         assertEquals(4.0, dfp.getAveragePixelValue(finalImage), DELTA);
     }
+
+    @Test
+    public void testApplyComplexDarkFrame() {
+        double[][] image1 = new double[4][3];
+        image1[0][0] = 0.55;
+        image1[0][1] = 1.78;
+        image1[0][2] = 5.24;
+        image1[1][0] = 2.25;
+        image1[1][1] = 2.46;
+        image1[1][2] = 3.47;
+        image1[2][0] = 6.04;
+        image1[2][1] = 3.02;
+        image1[2][2] = 1.43;
+        image1[3][0] = 2.47;
+        image1[3][1] = 0.36;
+        image1[3][2] = 4.54;
+
+        double[][] image2 = new double[4][3];
+        image2[0][0] = 1.25;
+        image2[0][1] = 3.48;
+        image2[0][2] = 2.44;
+        image2[1][0] = 0.25;
+        image2[1][1] = 1.47;
+        image2[1][2] = 2.37;
+        image2[2][0] = 4.14;
+        image2[2][1] = 1.42;
+        image2[2][2] = 0.75;
+        image2[3][0] = 1.64;
+        image2[3][1] = 3.664;
+        image2[3][2] = 1.554554;
+
+        double[][] image3 = new double[4][3];
+        image3[0][0] = 1.5;
+        image3[0][1] = 4.7845;
+        image3[0][2] = 8.2;
+        image3[1][0] = 3.255456;
+        image3[1][1] = 4.444554;
+        image3[1][2] = 2.45545;
+        image3[2][0] = 4.5452;
+        image3[2][1] = 1.54515;
+        image3[2][2] = 2.4;
+        image3[3][0] = 1.47545;
+        image3[3][1] = 5.54;
+        image3[3][2] = 1.54;
+
+        double[][] processedImage = new double[4][3];
+        processedImage[0][0] = 50.545;
+        processedImage[0][1] = 70.75;
+        processedImage[0][2] = 16.2;
+        processedImage[1][0] = 14.155;
+        processedImage[1][1] = 8.4444;
+        processedImage[1][2] = 45.445;
+        processedImage[2][0] = 16.542;
+        processedImage[2][1] = 25.545;
+        processedImage[2][2] = 24.25445;
+        processedImage[3][0] = 60.4755;
+        processedImage[3][1] = 54.54;
+        processedImage[3][2] = 25.54;
+
+        List<double[][]> images = new ArrayList<>();
+        images.add(image1);
+        images.add(image2);
+        images.add(image3);
+
+        double[][] darkFrame = dfp.createDarkFrame(images);
+
+        double[][] finalImage = dfp.applyDarkFrame(processedImage, darkFrame);
+
+        assertEquals(31.599576, dfp.getAveragePixelValue(finalImage), DELTA);
+    }
 }
